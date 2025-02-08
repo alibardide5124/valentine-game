f4a88cff6ca52e86cd407879f8cf25461a5e7ec4
Ali Bardide <alibardide5124@gmail.com>
2025-02-08 15:49:23 +0330

    Initial Commit

---
+++ app/src/main/java/com/phoenix/valentine/screen/gameboy/GameBoyRoute.kt
@@ -0,0 +1,8 @@
+package com.phoenix.valentine.screen.gameboy
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GameBoyRoute() {
+    GameBoyScreen()
+}
---
+++ app/src/main/java/com/phoenix/valentine/screen/gameboy/GameBoyScreen.kt
@@ -0,0 +1,9 @@
+package com.phoenix.valentine.screen.gameboy
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GameBoyScreen() {
+
+}
